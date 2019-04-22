6a5004116041a986d194253d426de1a9d65c93f3
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 01:37:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   new_array=[]
   num_of_hotdogs_eaten= 0
   while num_of_hotdogs_eaten < array.length
-  num_of_hotdogs_eaten += 1
     new_array.push(array[num_of_hotdogs_eaten] ** 2)
+    num_of_hotdogs_eaten += 1
+    
   end
   new_array
 end
